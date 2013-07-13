831a5943ee73037c64632a9c5ab070d61c0b545d
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-07-12 20:52:04 -0700

    adding support for downloading sent messages, as well as recieved. working great, so far

--- models/messages.lisp
+++ models/messages.lisp
@@ -7,36 +7,46 @@
    ("keys" :type sequence :required t)
    ("data" :type cl-async-util:bytes-or-string)))
 
-(defafun get-messages-for-persona (future) (persona-id challenge-response &key (after ""))
+(defafun get-messages-by-persona (future) (persona-id &key (after "") (index "get_messages_to"))
   "Gets messages for a persona. If a message ID is specified for :after, will
    only get messages after that ID."
+  (alet* ((sock (db-sock))
+          (query (r:r (:map
+                        (:eq-join
+                          (:between
+                            (:table "messages")
+                            :left (list persona-id (concatenate 'string after ".")) ;; moar hax
+                            :right (list persona-id "zzzzzzzzzzzzzzzzzzzzzzzzz")    ;; lol h4x
+                            :index index)
+                          "from"
+                          (:table "personas"))
+                        (r:fn (row)
+                          (:without
+                            (:merge
+                              (:merge
+                                row
+                                (:attr row "left"))
+                              `(("persona" . ,(:without (:attr row "right") "secret"))))
+                            "left"
+                            "right")))))
+          (cursor (r:run sock query))
+          (res (r:to-array sock cursor)))
+    (if (r:cursorp cursor)
+        (wait-for (r:stop sock cursor)
+          (r:disconnect sock))
+        (r:disconnect sock))
+    (finish future res)))
+
+(defafun get-messages-for-persona (future) (persona-id challenge-response &key (after ""))
+  "Get messages (both sent and received) for a persona. Optionally allows to
+   only grab messages after a specific message ID."
   (aif (persona-challenge-response-valid-p persona-id challenge-response)
-       (alet* ((sock (db-sock))
-               (query (r:r (:map
-                             (:eq-join
-                               (:between
-                                 (:table "messages")
-                                 :left (list persona-id (concatenate 'string after ".")) ;; moar hax
-                                 :right (list persona-id "zzzzzzzzzzzzzzzzzzzzzzzzz")    ;; lol h4x
-                                 :index "get_messages")
-                               "from"
-                               (:table "personas"))
-                             (r:fn (row)
-                               (:without
-                                 (:merge
-                                   (:merge
-                                     row
-                                     (:attr row "left"))
-                                   `(("persona" . ,(:without (:attr row "right") "secret"))))
-                                 "left"
-                                 "right")))))
-               (cursor (r:run sock query))
-               (res (r:to-array sock cursor)))
-         (if (r:cursorp cursor)
-             (wait-for (r:stop sock cursor)
-               (r:disconnect sock))
-             (r:disconnect sock))
-         (finish future res))
+       (alet ((to-persona (get-messages-by-persona persona-id :after after :index "get_messages_to"))
+              (from-persona (get-messages-by-persona persona-id :after after :index "get_messages_from"))
+              (hash (make-hash-table :test #'equal)))
+         (setf (gethash "received" hash) to-persona
+               (gethash "sent" hash) from-persona)
+         (finish future hash))
        (signal-error future (make-instance 'insufficient-privileges
                                            :msg "Sorry, either the persona you are getting messages for doesn't exist or you don't have access to it."))))
 
