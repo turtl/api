2146e03bdea2f02295ff7df341b3a6a84e7e6508
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-05-23 14:41:42 -0700

    adding promo codes table

--- config/schema.lisp
+++ config/schema.lisp
@@ -49,7 +49,8 @@
                              (:attr s "type")
                              (:attr s "id") )))
         :rel (:version 1 :multi t)))
-    :log (:indexes (:hash (:version 1))))
+    :log (:indexes (:hash (:version 1)))
+    :promo (:indexes (:code (:version 1))))
   "Holds our entire db/table/index schema. Tables are are created if they don't
    exist. Indexes are also created/updated if they don't exist or if the version
    doesn't match. Index names store the version in them, ie the index `user_id`
