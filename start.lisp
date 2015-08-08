9bc6f5730b35ddae9a3628c2005e0d604c7b4c70
Andrew Danger Lyon <orthecreedence@gmail.com>
2015-08-08 10:13:01 -0700

    making error handling dependant on *prod-err-handl*

--- start.lisp
+++ start.lisp
@@ -1,7 +1,5 @@
 (ql:quickload :turtl)
 (vom:config t :info)
 (vom:config :cl-rethinkdb :warn)
-(let ((blackbird:*debug-on-error* t)
-      (wookie-config:*debug-on-error* t))
-  (turtl:start :port 8182))
+(turtl:start :port 8182)
 
