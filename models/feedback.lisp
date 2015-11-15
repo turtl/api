c03decc8cf40f0e544d6f6afcd7c3412287d1892
Andrew Danger Lyon <orthecreedence@gmail.com>
2015-11-14 19:16:46 -0800

    adding nobody@turtl.it to non-emailed feedback

--- models/feedback.lisp
+++ models/feedback.lisp
@@ -2,13 +2,15 @@
 
 (defvalidator validate-feedback
   (("user_id" :type id :required t)
-   ("email" :type string)
+   ("email" :type string :required t)
    ("body" :type string :required t))
   :old t)
 
 (defafun send-feedback (future) (user-id feedback-data)
   "Send feedback posted by a user to the proper channels (email, most likely)."
   (setf (gethash "user_id" feedback-data) user-id)
+  (unless (gethash "email" feedback-data)
+    (setf (gethash "email" feedback-data) "nobody@turtl.it"))
   (validate-feedback (feedback-data future)
     (alet ((sentp (email-feedback feedback-data)))
       (finish future sentp))))
