37e4d2ae3d01ffd4c03b8e8d89f6c20779052f08
Andrew Danger Lyon <orthecreedence@gmail.com>
2016-02-24 01:42:38 -0800

    add-server-log: ignore socket eof/timeout conditions. nobody cares. really.

--- models/log.lisp
+++ models/log.lisp
@@ -15,13 +15,14 @@
   "Add a server log entry (when we catch errors that shouldn't necessarily
    be returned to he user). This is a simple wrapper around add-log."
   (vom:error "server caught error: (~a): ~a" location err)
-  (let ((log-data (make-hash-table :test #'equal)))
-    (setf (gethash "msg" log-data) (format nil "~a" err)
-          (gethash "url" log-data) location
-          (gethash "line" log-data) "0"
-          (gethash "version" log-data) "api")
-    (alet ((res (add-log log-data)))
-      (finish future res))))
+  (unless (typep err '(or as:streamish-eof as:socket-timeout))
+    (let ((log-data (make-hash-table :test #'equal)))
+      (setf (gethash "msg" log-data) (format nil "~a" err)
+            (gethash "url" log-data) location
+            (gethash "line" log-data) "0"
+            (gethash "version" log-data) "api")
+      (alet ((res (add-log log-data)))
+        (finish future res)))))
 
 (defafun add-log (future) (log-data)
   "Add a log entry to the DB."
